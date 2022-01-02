428b996518f02ef92eb399e1cec306c85f691fa3
TaeYeongJ <dev.taeyeong@gmail.com>
2022-01-02 17:23:13 +0900

    test

--- js/pageContent.js
+++ js/pageContent.js
@@ -6,8 +6,6 @@
  * 그렇지 않으면 스타일을 취소하십시오.
  */
 (function() {
-    // increaseAccessCount(document.URL)
-
     if (window.innerWidth > 770) {
 
         var sidebarWrap = document.querySelector('.right>.wrap')
